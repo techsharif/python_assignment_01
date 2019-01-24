2f990d35bdec275313d649c734b94c77b8ece1b1
sharif <sharif.chowdhury@circlefintech.com>
2019-01-24 14:18:12 +0600

    create assignment

---
+++ functions.py
@@ -0,0 +1,18 @@
+def readAllData(file_name):
+	with open(file_name) as file:
+		# write your code here
+
+		return []
+
+def computeAverageForClasses(data):
+	average = {}
+	# write your code here
+
+	return average
+
+def misclassified(data):
+	misclassified = []
+	# write your code here
+
+	return misclassified
+
